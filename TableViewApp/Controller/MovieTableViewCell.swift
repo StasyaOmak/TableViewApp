8d5c7cd06216b5fa9c1f09d0709ce2f1691dfadf
Anastasiya <ios.developer.by@gmail.com>
2023-10-28 11:58:01 +0300

    Class work

---
+++ TableViewApp/Controller/MovieTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieTableViewCell.swift
+//  TableViewApp
+//
+//  Created by Anastasiya Omak on 28/10/2023.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieImageView: UIImageView!    
+    @IBOutlet weak var movieLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TableViewApp/Model/Movie.swift
@@ -0,0 +1,11 @@
+//
+//  Movie.swift
+//  TableViewApp
+//
+//  Created by Anastasiya Omak on 27/10/2023.
+//
+
+import Foundation
+
+
+
